7f2977cc4b8796cb6274c1b7f002c15b889edd20
Alex Simisi <smsxander@gmail.com>
2019-06-24 22:37:05 +0200

    Installation de composer annotations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,14 +1,26 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    public function index()
+    public function index():Response
     {
         //return new Response("<h1>Bonjour</h1>");
         return $this->render('index.html.twig');
+    }
+
+    /**
+     * Undocumented function
+     * @Route ("/contact", name= "contact")
+     * @return void
+     */
+    public function contact()
+    {
+        return $this->render('contact.html.twig');
     }
 }
 
